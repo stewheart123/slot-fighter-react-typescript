5c2f44cff7ce9f98ffa1cf7f614a38ab68ea6ae8
Stewart <44583600+stewheart123@users.noreply.github.com>
2023-07-01 17:42:53 +0100

    reel container into animation

--- src/states/mainGameIntroState/steps/setupMainBackgroundStep.ts
+++ src/states/mainGameIntroState/steps/setupMainBackgroundStep.ts
@@ -8,6 +8,7 @@ import {
   Sprite,
   Spritesheet,
   Texture,
+  Ticker,
 } from "pixi.js";
 import initializeApp from "../../../initializer";
 import assets from "../../../models/Assets";
@@ -16,7 +17,7 @@ import  floatingSignal  from "../../../signal";
 export class SetupBackgroundStep implements IStep {
   public isComplete = false;
   public app = initializeApp();
-
+ 
   public start(signal: Signal): void {
     const mainSceneContainer = new Container();
     const stageOneBackground = new Sprite();
@@ -24,36 +25,28 @@ export class SetupBackgroundStep implements IStep {
     const reelContainer = new Container();
     reelContainer.pivot.set(0.5,0.5);
     reelContainer.position.set(this.app.screen.width / 2- (110 * 2), 0);
+for(let x = 0; x < 4; x++) {
+  const tempReel = new Container();
 
-    for(let x = 0; x < 5; x++) {
-      let randomSymbolIndex = Math.floor(Math.random() * 10);
-      console.log(randomSymbolIndex);
-      const tempReel = new Container();
-      const tempSymbol = new Sprite();
-      tempSymbol.texture = assets.symbolTextures[randomSymbolIndex];
-      tempSymbol.width = 110;
-      tempSymbol.height = 110;
-      tempSymbol.position.set(0, (x * 110), );
-      tempReel.addChild(tempSymbol);
-      reelContainer.addChild(tempReel);
-    }
+  for(let y = 0; y < 4; y++) {
+    let randomSymbolIndex = Math.floor(Math.random() * 10);
+    //console.log(randomSymbolIndex);
+    const tempSymbol = new Sprite();
+    tempSymbol.texture = assets.symbolTextures[randomSymbolIndex];
+    tempSymbol.width = 110;
+    tempSymbol.height = 110;
+    tempSymbol.position.set((x*110), (y * 110), );
+    tempReel.addChild(tempSymbol);
+    tempReel.addChild(tempReel);
+  }
+  reelContainer.addChild(tempReel);
+}
+reelContainer.position.set((this.app.view.width / 2 - (110*2)) ,-50);
+
+
     stageOneBackground.texture = assets.textures[4]; //6 - 16 random number
     stageOneBackground.width = this.app.screen.width;
     stageOneBackground.height = this.app.screen.height;
-
-    //reels - build it  for the first time, use another state to re-enter the spin ready state
-    
-
-    // const reelOne = new Container();
-    //  const symbolOne = new Sprite();
-    //  symbolOne.width = 110;
-    //  symbolOne.height = 110;
-    //  symbolOne.position.set(0,0);
-    //  symbolOne.texture = assets.textures[6];
-    //  reelOne.addChild(symbolOne);
-    //  reelContainer.addChild(reelOne);
-    //  console.log(mainSceneContainer);
-     //end of reels
      
      mainSceneContainer.addChild(stageOneBackground);
      mainSceneContainer.addChild(reelContainer);
@@ -63,9 +56,24 @@ export class SetupBackgroundStep implements IStep {
       // add a pixi game instruction in here, the floating signal canbe exported to the UI
       console.log('floating signal');
     });
+    
+    console.log(reelContainer.position.y);
+    const rotateAnimation = ():void => {
+      
+        reelContainer.position.y += 2;
+        this.app.renderer.render(this.app.stage); // must include this to update the visuals!!!
+        if(reelContainer.position.y > 60) {
+          ticker.stop();
+        }
+    }
+    const ticker = new Ticker();
+
+    ticker.add(rotateAnimation);
+    ticker.start();
+    // how to set up an animation?? 
 
     this.isComplete = true;
-    signal.dispatch();
-    console.log(signal);
+    //signal.dispatch();
+    //console.log(signal);
   }
 }
